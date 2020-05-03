f524b19632b73c66aa23a7b367f872dda38e8166
Mattias Ek <mattias.ek@bristol.ac.uk>
2020-05-03 21:22:23 +0100

    doc

--- docs/conf.py
+++ docs/conf.py
@@ -19,9 +19,7 @@
 #
 import os
 import sys
-import isopy
 sys.path.insert(0, os.path.abspath('./../'))
-sys.path.insert(0, isopy.__path__)
 
 
 # -- General configuration ------------------------------------------------
