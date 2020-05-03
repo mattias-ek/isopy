221a5990aa8517951b4f4125c425414891648070
Mattias Ek <mattias.ek@bristol.ac.uk>
2020-05-03 21:17:13 +0100

    doc

--- docs/conf.py
+++ docs/conf.py
@@ -19,14 +19,16 @@
 #
 import os
 import sys
+import isopy
 sys.path.insert(0, os.path.abspath('./../'))
+sys.path.insert(0, isopy.__path__)
 
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '1.8'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
